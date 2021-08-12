a5db446d188094ea84f65a467d98c1d1669ee35a
mareadelmar <mar.ortega.v@gmail.com>
2021-08-12 20:29:43 -0300

    codemirror

--- main.js
+++ main.js
@@ -3,9 +3,51 @@ const cssEl = document.querySelector("#css");
 const jsEl = document.querySelector("#js");
 const iframeEl = document.querySelector("#results");
 
+const htmlContainer = CodeMirror(htmlEl, {
+    lineNumbers: true,
+    indentUnit: 4,
+    tabSize: 4,
+    lineWrapping: true,
+    mode: "xml",
+    stylesheet: "codemirror/lib/codemirror/css/xmlcolors.css",
+    parserfile: "codemirror/parsexml.js",
+    path: "codemirror/lib/codemirror/js/",
+    theme: "dracula",
+    autoCloseTags: true,
+});
+
+const cssContainer = CodeMirror(cssEl, {
+    lineNumbers: true,
+    indentUnit: 4,
+    tabSize: 4,
+    lineWrapping: true,
+    mode: "css",
+    stylesheet: "codemirror/lib/codemirror/css/csscolors.css",
+    parserfile: "codemirror/parsecss.js",
+    path: "codemirror/lib/codemirror/js/",
+    theme: "dracula",
+    autoCloseBrackets: true,
+});
+
+const jsContainer = CodeMirror(jsEl, {
+    lineNumbers: true,
+    indentUnit: 4,
+    tabSize: 4,
+    lineWrapping: true,
+    mode: "javascript",
+    stylesheet: "codemirror/lib/codemirror/css/jscolors.css",
+    parserfile: "codemirror/parsejs.js",
+    path: "codemirror/lib/codemirror/js/",
+    theme: "dracula",
+    autoCloseBrackets: true,
+});
+
 htmlEl.addEventListener("input", setResults);
 cssEl.addEventListener("input", setResults);
 jsEl.addEventListener("input", setResults);
+
+console.log(htmlEl.value);
+console.log(htmlContainer, htmlContainer.getValue());
 
 Split({
     columnGutters: [
@@ -22,10 +64,17 @@ Split({
     ],
 });
 
+//setResults();
+function setResults() {
+    const result = createHtml();
+
+    iframeEl.setAttribute("srcdoc", result);
+}
+
 function createHtml() {
-    const html = htmlEl.value;
-    const css = cssEl.value;
-    const js = jsEl.value;
+    const html = htmlContainer.getValue();
+    const css = cssContainer.getValue();
+    const js = jsContainer.getValue();
     console.log(html, css, js);
 
     return `
@@ -44,10 +93,4 @@ function createHtml() {
         </body>
     </html>
     `;
-}
-
-function setResults() {
-    const result = createHtml();
-
-    iframeEl.setAttribute("srcdoc", result);
 }
