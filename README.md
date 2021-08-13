780d52beb0af8ae20e09699beb7064aba8bdf380
mareadelmar <mar.ortega.v@gmail.com>
2021-08-13 09:05:00 -0300

    arreglo logos de lenguaje y readme

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # editor de código estilo codePen.
 
-Con html, css y js vainilla.
+Con html, css y js.
+Librería Split.js para redimensionar el editor y CodeMirror como editor de código.
+
+Deploy: [https://codiguito-lapiz.vercel.app](https://codiguito-lapiz.vercel.app).
--- main.js
+++ main.js
@@ -3,51 +3,44 @@ const cssEl = document.querySelector("#css");
 const jsEl = document.querySelector("#js");
 const iframeEl = document.querySelector("#results");
 
-const htmlContainer = CodeMirror(htmlEl, {
+const configCodeMirror = {
     lineNumbers: true,
     indentUnit: 4,
     tabSize: 4,
     lineWrapping: true,
+    theme: "dracula",
+};
+
+const htmlContainer = CodeMirror(htmlEl, {
+    ...configCodeMirror,
     mode: "xml",
     stylesheet: "codemirror/lib/codemirror/css/xmlcolors.css",
     parserfile: "codemirror/parsexml.js",
     path: "codemirror/lib/codemirror/js/",
-    theme: "dracula",
     autoCloseTags: true,
 });
 
 const cssContainer = CodeMirror(cssEl, {
-    lineNumbers: true,
-    indentUnit: 4,
-    tabSize: 4,
-    lineWrapping: true,
+    ...configCodeMirror,
     mode: "css",
     stylesheet: "codemirror/lib/codemirror/css/csscolors.css",
     parserfile: "codemirror/parsecss.js",
     path: "codemirror/lib/codemirror/js/",
-    theme: "dracula",
     autoCloseBrackets: true,
 });
 
 const jsContainer = CodeMirror(jsEl, {
-    lineNumbers: true,
-    indentUnit: 4,
-    tabSize: 4,
-    lineWrapping: true,
+    ...configCodeMirror,
     mode: "javascript",
     stylesheet: "codemirror/lib/codemirror/css/jscolors.css",
     parserfile: "codemirror/parsejs.js",
     path: "codemirror/lib/codemirror/js/",
-    theme: "dracula",
     autoCloseBrackets: true,
 });
 
 htmlEl.addEventListener("input", setResults);
 cssEl.addEventListener("input", setResults);
 jsEl.addEventListener("input", setResults);
-
-console.log(htmlEl.value);
-console.log(htmlContainer, htmlContainer.getValue());
 
 Split({
     columnGutters: [
@@ -64,10 +57,8 @@ Split({
     ],
 });
 
-//setResults();
 function setResults() {
     const result = createHtml();
-
     iframeEl.setAttribute("srcdoc", result);
 }
 
@@ -75,7 +66,6 @@ function createHtml() {
     const html = htmlContainer.getValue();
     const css = cssContainer.getValue();
     const js = jsContainer.getValue();
-    console.log(html, css, js);
 
     return `
     <!DOCTYPE html>
